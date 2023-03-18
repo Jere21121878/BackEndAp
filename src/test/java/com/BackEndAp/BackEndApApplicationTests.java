27a17b7e48d14bd8d4b38ab33b6057a7c116f3ea
Jere21121878 <105524917+Jere21121878@users.noreply.github.com>
2023-03-18 17:48:28 -0300

    back

--- src/test/java/com/BackEndAp/BackEndApApplicationTests.java
+++ src/test/java/com/BackEndAp/BackEndApApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.mgb;
+package com.BackEndAp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MgbApplicationTests {
+@SpringBootTest(classes = BackEndApApplicationTests.class  )
+class BackEndApApplicationTests {
 
 	@Test
 	void contextLoads() {
