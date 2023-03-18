75a0e7d5e825a00786fdbcfead14fb6aa871509f
Jere21121878 <105524917+Jere21121878@users.noreply.github.com>
2023-03-18 17:12:15 -0300

    parte del back

---
+++ src/main/java/com/BackEndAp/BackEndApApplication.java
@@ -0,0 +1,13 @@
+package com.BackEndAp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApApplication.class, args);
+	}
+
+}
